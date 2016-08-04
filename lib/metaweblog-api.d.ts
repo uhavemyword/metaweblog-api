f2a6fac08cf66d928e69921f969193e4c38bcb4e
Ben <peng-chen@live.com>
2016-08-05 00:29:43 +0800

    fix typing definition and add test case in typescript

--- lib/metaweblog-api.d.ts
+++ lib/metaweblog-api.d.ts
@@ -5,29 +5,29 @@
 // Definitions by: Peng Chen <https://github.com/uhavemyword>
 
 declare class MetaWeblog {
-    constructor(options: string | any);
-    getUsersBlogs(appKey: string, username: string, password: string): Promise<MetaWeblog.BlogInfo[]>;
-    getRecentPosts(blogid: string, username: string, password: string, numberOfPosts: number): Promise<MetaWeblog.Post[]>;
-    getCategories(blogid: string, username: string, password: string): Promise<MetaWeblog.CategoryInfo[]>;
-    getPost(postid: string, username: string, password: string): Promise<MetaWeblog.Post>;
-    editPost(postid: string, username: string, password: string, post: MetaWeblog.Post): Promise<boolean>;
-    newPost(blogid: string, username: string, password: string, post: MetaWeblog.Post, publish: boolean): Promise<number>;
-    deletePost(appKey: string, postid: string, username: string, password: string, publish: boolean): Promise<boolean>;
-    newMediaObject(blogid: string, username: string, password: string, mediaObject: MetaWeblog.MediaObject): Promise<MetaWeblog.MediaObject>;
-}
+        constructor(options: string | any);
+        getUsersBlogs(appKey: string, username: string, password: string): Promise<MetaWeblog.BlogInfo[]>;
+        getRecentPosts(blogid: string, username: string, password: string, numberOfPosts: number): Promise<MetaWeblog.Post[]>;
+        getCategories(blogid: string, username: string, password: string): Promise<MetaWeblog.CategoryInfo[]>;
+        getPost(postid: string, username: string, password: string): Promise<MetaWeblog.Post>;
+        editPost(postid: string, username: string, password: string, post: MetaWeblog.Post, publish: boolean): Promise<boolean>;
+        newPost(blogid: string, username: string, password: string, post: MetaWeblog.Post, publish: boolean): Promise<number>;
+        deletePost(appKey: string, postid: string, username: string, password: string, publish: boolean): Promise<boolean>;
+        newMediaObject(blogid: string, username: string, password: string, mediaObject: MetaWeblog.MediaObject): Promise<MetaWeblog.MediaObject>;
+    }
 
 declare namespace MetaWeblog {
-    export interface BlogInfo {
+    interface BlogInfo {
         blogid: string;
         url: string;
         blogName: string;
     }
 
-    export interface Post {
-        dateCreated: Date;
+    interface Post {
         description: string;
         title: string;
         categories?: string[];
+        dateCreated?: Date;
         link?: string;
         permalink?: string;
         postid?: string;
@@ -35,7 +35,7 @@ declare namespace MetaWeblog {
         userid?: string;
     }
 
-    export interface CategoryInfo {
+    interface CategoryInfo {
         description: string;
         htmlUrl: string;
         rssUrl: string;
@@ -43,19 +43,21 @@ declare namespace MetaWeblog {
         categoryid: string;
     }
 
-    export interface MediaObject {
+    interface MediaObject {
         name: string;
         type: string;
         bits: string;
     }
 
-    export interface Source {
+    interface Source {
         name?: string;
         url?: string;
     }
 
-    export interface Error {
+    interface Error {
         faultCode: string;
         faultString: string;
     }
 }
+
+export = MetaWeblog;
---
+++ tests/typescript.ts
@@ -0,0 +1,86 @@
+/// <reference path="./../node_modules/@types/node/index.d.ts" />
+
+const appKey = '';
+const username = 'MetaWeblog'; // use your username instead.
+const password = 'Test_0123';
+
+import fs = require("fs");
+import MetaWeblog = require('./../lib/metaweblog-api');
+const metaWeblog = new MetaWeblog('http://www.cnblogs.com/' + username + '/services/metaweblog.aspx');
+
+let blogid: string;
+let postid: string;
+
+// getUsersBlogs
+metaWeblog.getUsersBlogs(appKey, username, password)
+    .then(blogInfos => {
+        console.log('\n Method response for \'getUsersBlogs\': ');
+        console.log(blogInfos);
+        blogid = blogInfos[0].blogid;
+
+        // getCategories
+        metaWeblog.getCategories(blogid, username, password)
+            .then(categories => {
+                console.log('\n Method response[0] for \'getCategories\': ');
+                console.log(categories[0]);
+            });
+
+        // getRecentPosts
+        metaWeblog.getRecentPosts(blogid, username, password, 1)
+            .then(posts => {
+                console.log('\n Method response for \'getRecentPosts\': ');
+                console.log(posts);
+                postid = posts[0].postid;
+
+                // getPost
+                metaWeblog.getPost(postid, username, password)
+                    .then(post => {
+                        console.log('\n Method response for \'getPost\': ');
+                        console.log(post);
+
+                        // editPost
+                        post.description = 'Post updated by `MetaWeblog API` on ' + Date() + '<br />' + post.description;
+                        metaWeblog.editPost(postid, username, password, post, true)
+                            .then(success => {
+                                console.log('\n Method response for \'editPost\': ');
+                                console.log(success);
+                            });
+                    });
+            });
+
+        // newPost
+        let post: MetaWeblog.Post = {
+            title: 'New Post',
+            description: 'Post created by `MetaWeblog API` on ' + Date(),
+            categories: ['[Markdown]']
+        }
+
+        metaWeblog.newPost(blogid, username, password, post, true)
+            .then(newPostId => {
+                console.log('\n Method response for \'newPost\': ');
+                console.log(newPostId);
+
+                // deletePost
+                metaWeblog.deletePost(appKey, newPostId.toString(), username, password, true)
+                    .then(success => {
+                        console.log('\n Method response for \'deletePost\': ');
+                        console.log(success);
+                    });
+            });
+
+        // newMediaObject
+        var imageFile = fs.readFileSync('tests/test.jpg', 'binary');
+        var media = {
+            name: 'test.jpg',
+            type: 'image/jpg',
+            bits: imageFile
+        }
+        metaWeblog.newMediaObject(blogid, username, password, media)
+            .then(urlData => {
+                console.log('\n Method response for \'newMediaObject\': ');
+                console.log(urlData);
+            });
+    })
+    .catch(error => {
+        console.error(error);
+    });
